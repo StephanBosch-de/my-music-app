6b286927202001c0a0854af0ba334e522726d9a5
Stephan <stephanbosch@gmx.de>
2019-08-25 13:48:11 +0200

    first commit

---
+++ src/main/java/com/stbo/myprojects/mymusicapp/App.java
@@ -0,0 +1,13 @@
+package com.stbo.myprojects.mymusicapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
