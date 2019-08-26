042ee6227ea0afd9a6e5311791cea102a8d6df90
Stephan <stephanbosch@gmx.de>
2019-08-26 16:39:14 +0200

    Added Lombok Dependency and some JUnit tests

--- src/test/java/com/stbo/myprojects/mymusicapp/AppTest.java
+++ src/test/java/com/stbo/myprojects/mymusicapp/AppTest.java
@@ -1,38 +1,31 @@
 package com.stbo.myprojects.mymusicapp;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+public class AppTest {
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	@Test
+	public void MusicPieceConstructorTest() {
+		// Testing the Constructor of the Class MusicPiece
+		MusicPiece newPiece = new MusicPiece(1.55, "TestSong", "TestArtist");
+		assertEquals(newPiece, new MusicPiece(1.55, "TestSong", "TestArtist"));
+	}
+
+	@Test
+	public void addMusicPieceToListTest() {
+		// Testing if elements are added to the List
+		MusicPieceDatabase testDatabase = new MusicPieceDatabase();
+		testDatabase.addMusicPiece(new MusicPiece(3.20, "Lemon Tree", "Fool's Garden"));
+		testDatabase.addMusicPiece(new MusicPiece(5.20, "Torn", "Natalie Imbruglia"));
+
+		MusicPiece entry0 = new MusicPiece(3.20, "Lemon Tree", "Fool's Garden");
+		MusicPiece entry1 = new MusicPiece(5.20, "Torn", "Natalie Imbruglia");
+
+		assertEquals(entry0, testDatabase.MusicPieceList.get(0), "Test if values of entry0 equal with values of list element 0");
+		assertEquals(entry1, testDatabase.MusicPieceList.get(1), "Test if values of entry1 equal with values of list element 1");
+
+	}
+
 }
